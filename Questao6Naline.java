973b4d8fe7f8480f4ae02ba8a6f97201d9f6df40
Naline Belo <62017582+Nahline@users.noreply.github.com>
2022-05-04 21:58:56 -0300

    Update Questao6Naline.java

--- Questao6Naline.java
+++ Questao6Naline.java
@@ -1,6 +1,6 @@
-class Questao6Naline {  
-        public static void main(String args[]) { 
-          System.out.println("Nome: Naline Belo."); 
-          System.out.println("Time: Flamengo."); 
-        } 
-      }
+class Questao6Naline {
+  public static void main(String args[]) {
+    System.out.println("Nome: Naline Belo.");
+    System.out.println("Time: Flamengo.");
+  }
+}

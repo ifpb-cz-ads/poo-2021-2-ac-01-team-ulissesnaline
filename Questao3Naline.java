d945acb7abfa2ee1dcffc5a6e1db1642aefbc0ee
Naline Belo <62017582+Nahline@users.noreply.github.com>
2022-05-04 21:58:39 -0300

    Update Questao3Naline.java

--- Questao3Naline.java
+++ Questao3Naline.java
@@ -1,5 +1,5 @@
-class Questao3Naline {  
-    public static void main(String args[]) { 
-   System.out.println("Terminei a primeira aula com um programa java!"); 
-  } 
-  } 
+class Questao3Naline {
+  public static void main(String args[]) {
+    System.out.println("Terminei a primeira aula com um programa java!");
+  }
+}
